4b9bb0735cc2e0f7690e27d6ccd647e33a6e9eb3
bjorneboman <gostabohman@gmail.com>
2025-10-27 10:29:41 +0100

    Updates file references to profile images

--- bjorn.js
+++ bjorn.js
@@ -7,16 +7,14 @@ const scanOverlayImage = document.createElement("img")
 const scanOverlayMask = document.createElement("div")
 
 // Virtual elements starting styles
-scanOverlayImage.src = `/assets/images/bjorn-boman.jpg`
-scanOverlayImage.style = `width: 4.5rem; height: 4.5rem;`
+scanOverlayImage.src = `/assets/images/tn-bjorn-boman.jpg`
 scanOverlayMask.style = `mask-image: linear-gradient(#000);
 width: 100%;
-height: 1rem;
+height: 2rem;
 position: absolute;
 top: -1rem;
 left: 0;
 `
-
 
 // Animation
 
@@ -24,53 +22,53 @@ let startTime
 let reqID
 
 function frame(timestamp) {
-if (!startTime) {
+  if (!startTime) {
     startTime = timestamp
-}
-const elapsedTime = timestamp - startTime
+  }
+  const elapsedTime = timestamp - startTime
 
-let shift = Math.min(0.1 * elapsedTime, 100)
-let ran = Math.random()
+  let shift = Math.min(0.1 * elapsedTime, 100)
+  let ran = Math.random()
 
-container.style.filter = `brightness(${ran+1})`
-container.style.backgroundPosition = `center ${(Math.max((ran - 0.95), 0)/.05*200)-10}px`
+  container.style.filter = `brightness(${ran * 1.1})`
+  container.style.backgroundPosition = `center ${
+    (Math.max(ran - 0.95, 0) / 0.05) * 200 - 10
+  }px`
 
-scanOverlayMask.style.transform = `translateY(${shift}px)`
-scanOverlayImage.style = `transform: 
-translateX(0px)
-translateY(${shift * -1}rem) 
-skewX(${(ran - 0.5) * 20}deg);
-scale: ${ran * 0.1 + 1} 1;
-filter: hue-rotate(${(Math.max(ran - 0.5, 0) * 720) / 0.5}deg)`
+  scanOverlayMask.style.transform = `translateY(${shift}px)`
+  scanOverlayImage.style = `width: 4.5rem; height: 4.5rem; transform: translateX(0px) translateY(calc(1rem - ${shift}px)) skewX(${
+    (ran - 0.5) * 20
+  }deg); scale = ${ran * 0.1 + 1} 1; 
+  filter: hue-rotate(${(Math.max(ran - 0.5, 0) * 720) / 0.5}deg)`
 
-if (shift < 100) {
+  if (shift < 100) {
     reqID = requestAnimationFrame(frame)
-} else {
+  } else {
     shift = 0
     startTime = timestamp
     reqID = requestAnimationFrame(frame)
-}
+  }
 }
 
 // Mouse events
 
-container.addEventListener('mouseover', () => {
-container.append(scanOverlayMask)
-scanOverlayMask.append(scanOverlayImage)
+container.addEventListener("mouseover", () => {
+  container.append(scanOverlayMask)
+  scanOverlayMask.append(scanOverlayImage)
 
-reqID = requestAnimationFrame(frame)
+  reqID = requestAnimationFrame(frame)
 })
 
-container.addEventListener('mouseout', () => {
-window.cancelAnimationFrame(reqID)
-scanOverlayMask.removeChild(scanOverlayImage)
-container.removeChild(scanOverlayMask)
-container.style.backgroundPosition = "center"
-container.style.filter = ""
+container.addEventListener("mouseout", () => {
+  window.cancelAnimationFrame(reqID)
+  scanOverlayMask.removeChild(scanOverlayImage)
+  container.removeChild(scanOverlayMask)
+  container.style.backgroundPosition = "center"
+  container.style.filter = ""
 })
 
 container.addEventListener("dblclick", () => {
   window.confirm("Gå till Björns profil på Github?")
     ? window.open("https://github.com/bjorneboman")
-    : "Oh well";
-});
+    : "Oh well"
+})
